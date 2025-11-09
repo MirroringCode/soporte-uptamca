54495ed95938bdb2128fbf848532b50a024012df
mirroringcode <miguelolivar999@gmail.com>
2025-11-09 19:15:25 -0400

    [fix] para poder ejecutarse dentro de su carpeta

--- api/common/create_admin.py
+++ api/common/create_admin.py
@@ -1,3 +1,9 @@
+import os, sys
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_dir)
+
+
+
 from app import app
 from models import User, Rol
 from db import db
